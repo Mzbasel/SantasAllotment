ed4a81ce1c745902d153bd9f9e5fa92891c9b0bc
Mzbasel <mzbasel637936@gmail.com>
2023-01-16 15:46:44 +0000

    Did a little bit of refactoring

--- src/main/java/RandomAllocator.java
+++ src/main/java/RandomAllocator.java
@@ -6,43 +6,50 @@ import java.util.Map;
 
 public class RandomAllocator {
     private SecureRandom random = new SecureRandom();
-    private List<Integer> index = new ArrayList<>();
+    private List<Integer> indexList = new ArrayList<>();
+    private List<Integer> availableIndexes = new ArrayList<>();
     private Map<Integer, Integer> diagonallyLeftToRight = new HashMap<>();
     private Map<Integer, Integer> diagonallyRightToLeft = new HashMap<>();
-    private int leftToRightCount = 0;
-    private int rightToLeftCount = 0;
-
-    private int vertical = 0;
+    private int leftToRightCount = 0, rightToLeftCount = 0, vertical = 0;
+    private int index, randomIndex;
 
     public List<Integer> generate(int size) {
         createDiagonalObstacles(size);
+        createAvailableIndexes(size);
 
-        while(index.size()!= size){
-            int randomIndex = random.nextInt(size);
-            if(!index.contains(randomIndex)){
-                if(diagonallyRightToLeft.get(vertical)==randomIndex){
-                    rightToLeftCount++;
-                    if(rightToLeftCount < 2){
-                        index.add(randomIndex);
-                        vertical++;
-                        continue;
-                    }
-                    continue;
-                }
-                if(diagonallyLeftToRight.get(vertical)==randomIndex) {
-                    leftToRightCount++;
-                    if(leftToRightCount < 2) {
-                        index.add(randomIndex);
-                        vertical++;
-                        continue;
-                    }
-                    continue;
-                }
-                index.add(randomIndex);
-                vertical++;
+        while(indexList.size()!= size){
+            index = generateRandomIndex();
+            randomIndex = availableIndexes.get(index);
+            if(diagonallyRightToLeft.get(vertical)==randomIndex){
+                rightToLeftCount++;
+                if(rightToLeftCount < 2) addNewToIndexList();
+                continue;
             }
+            if(diagonallyLeftToRight.get(vertical)==randomIndex) {
+                leftToRightCount++;
+                if(leftToRightCount < 2) addNewToIndexList();
+                continue;
+            }
+            addNewToIndexList();
+
         }
-        return index;
+        return indexList;
+    }
+
+    private void addNewToIndexList() {
+        indexList.add(randomIndex);
+        availableIndexes.remove(index);
+        vertical++;
+    }
+
+    private int generateRandomIndex() {
+        return random.nextInt(availableIndexes.size());
+    }
+
+    private void createAvailableIndexes(int size) {
+        for (int i = 0; i < size; i++) {
+            availableIndexes.add(i);
+        }
     }
 
     private void createDiagonalObstacles(int size) {
--- src/test/java/RandomAllocatorTest.java
+++ src/test/java/RandomAllocatorTest.java
@@ -1,63 +1,121 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class RandomAllocatorTest {
 
     private RandomAllocator randomAllocator;
-
+    private List<Integer> notExpectedAllocation;
 
     @BeforeEach
     public void setUp(){
         randomAllocator = new RandomAllocator();
+        notExpectedAllocation = new ArrayList<>();
     }
 
-    public static Stream<Arguments> listOfNotExpectedAllocationsHorizontal() {
-        return Stream.of(
-                Arguments.of(List.of(0,0,0)),
-                Arguments.of(List.of(0,1,0)),
-                Arguments.of(List.of(0,2,0)),
-                Arguments.of(List.of(0,0,1))
-        );
-    }
-
-
-    @ParameterizedTest
-    @MethodSource("listOfNotExpectedAllocationsHorizontal")
-    public void should_not_allocate_trees_in_the_same_horizontal_line(List<Integer> notExpectedAllocation){
+   @Test
+    public void should_not_allocate_trees_in_the_same_horizontal_line_0_0_0(){
         int size = 3;
+        notExpectedAllocation = List.of(0,0,0);
 
         List<Integer> actualAllocation = randomAllocator.generate(size);
 
-        assertFalse(notExpectedAllocation.equals(actualAllocation));
+       assertNotEquals(notExpectedAllocation, actualAllocation);
     }
 
-    public static Stream<Arguments> listOfNotExpectedAllocationsDiagonalLeftToRight() {
-        return Stream.of(
-                Arguments.of(List.of(0,1,2,3)),
-                Arguments.of(List.of(0,1,3,2)),
-                Arguments.of(List.of(0,2,3,1)),
-                Arguments.of(List.of(2,1,3,0))
-        );
+    @Test
+    public void should_not_allocate_trees_in_the_same_horizontal_line_0_1_0(){
+        int size = 3;
+        notExpectedAllocation = List.of(0,1,0);
+
+        List<Integer> actualAllocation = randomAllocator.generate(size);
+
+        assertNotEquals(notExpectedAllocation, actualAllocation);
     }
 
+    @Test
+    public void should_not_allocate_trees_in_the_same_horizontal_line_0_2_0(){
+        int size = 3;
+        notExpectedAllocation = List.of(0,2,0);
 
-    @ParameterizedTest
-    @MethodSource("listOfNotExpectedAllocationsDiagonalLeftToRight")
-    public void should_not_allocate_trees_diagonally_left_to_right(List<Integer> notExpectedAllocation){
+        List<Integer> actualAllocation = randomAllocator.generate(size);
+
+        assertNotEquals(notExpectedAllocation, actualAllocation);
+    }
+
+    @Test
+    public void should_not_allocate_trees_in_the_same_horizontal_line_0_0_1(){
+        int size = 3;
+        notExpectedAllocation = List.of(0,0,1);
+
+        List<Integer> actualAllocation = randomAllocator.generate(size);
+
+        assertNotEquals(notExpectedAllocation, actualAllocation);
+    }
+
+    @Test
+    public void should_not_allocate_trees_in_the_same_horizontal_line_1_0_0(){
+        int size = 3;
+        notExpectedAllocation = List.of(1,0,0);
+
+        List<Integer> actualAllocation = randomAllocator.generate(size);
+
+        assertNotEquals(notExpectedAllocation, actualAllocation);
+    }
+
+    @Test
+    public void should_not_allocate_trees_in_the_same_horizontal_line_2_0_0(){
+        int size = 3;
+        notExpectedAllocation = List.of(2,0,0);
+
+        List<Integer> actualAllocation = randomAllocator.generate(size);
+
+        assertNotEquals(notExpectedAllocation, actualAllocation);
+    }
+
+    @Test
+    public void should_not_allocate_trees_diagonally_left_to_right_0_1_2_3(){
         int size = 4;
+        notExpectedAllocation = List.of(0,1,2,3);
 
         List<Integer> actualAllocation = randomAllocator.generate(size);
 
-        assertFalse(notExpectedAllocation.equals(actualAllocation));
+        assertNotEquals(notExpectedAllocation, actualAllocation);
+    }
+
+    @Test
+    public void should_not_allocate_trees_diagonally_left_to_right_0_1_3_2(){
+        int size = 4;
+        notExpectedAllocation = List.of(0,1,3,2);
+
+        List<Integer> actualAllocation = randomAllocator.generate(size);
+
+        assertNotEquals(notExpectedAllocation, actualAllocation);
+    }
+
+    @Test
+    public void should_not_allocate_trees_diagonally_left_to_right_0_2_3_1(){
+        int size = 4;
+        notExpectedAllocation = List.of(0,2,3,1);
+
+        List<Integer> actualAllocation = randomAllocator.generate(size);
+
+        assertNotEquals(notExpectedAllocation, actualAllocation);
+    }
+
+    @Test
+    public void should_not_allocate_trees_diagonally_left_to_right_2_1_3_0(){
+        int size = 4;
+        notExpectedAllocation = List.of(2,1,3,0);
+
+        List<Integer> actualAllocation = randomAllocator.generate(size);
+
+        assertNotEquals(notExpectedAllocation, actualAllocation);
     }
 
 }
